5880bf7ff59b835eeb2e3e6a9370259ce05f2857
Chiến Nguyễn <chienindustries@gmail.com>
2022-06-07 10:05:33 +0700

    ..

--- src/components/AlertPopup.js
+++ src/components/AlertPopup.js
@@ -2,11 +2,13 @@ import Swal from "sweetalert2";
 import thermo_icon from "./../assets/thermo.png";
 import dissolveOxy_icon from "./../assets/moist.png";
 import warning_icon from "./../assets/warning.png";
+// import Popup from 'reactjs-popup'
 import warning_red_icon from "./../assets/warning_red.png";
 import { ColorConstant } from "../value/color_constant";
 import { Constant } from "../value/constant";
 import pop_mp3 from "./../assets/pop.mp3";
 import error_mp3 from "./../assets/error.mp3";
+// import 'reactjs-popup/dist/index.css';
 function AlertPopup(sensor, title, isMute) {
   var imgSrc;
   let pop_audio = new Audio(error_mp3);
@@ -38,5 +40,6 @@ function AlertPopup(sensor, title, isMute) {
     confirmButtonColor: ColorConstant.mred,
     timer: Constant.timeSamplingData - 2000,
   });
+  
 }
 export default AlertPopup;
